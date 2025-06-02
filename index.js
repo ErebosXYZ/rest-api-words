8da307fa762c08fc3afeebc05d522f829d186c49
ErebosXYZ <robertmaynou@gmail.com>
2025-06-02 19:10:19 +0200

    it 1 i 2

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Cargar palabras desde archivo
-const words = require("./data/words.json");
+// const words = require("./data/words.json");
+let words;
+try {
+  words = require("./data/words.json");
+  console.log("words.json cargado correctamente");
+} catch (err) {
+  console.error("Error cargando words.json:", err);
+  words = [];
+}
 
 // Middleware
 app.use(morgan("dev"));
@@ -17,7 +25,32 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Iteración 1
 
+// app.get('/api/v1/words', (req, res) => {
+//   const randomIndex = Math.floor(Math.random() * words.length);
+
+//   res.json({ word: words[randomIndex] });
+// });
+
 // Iteración 2
+
+app.get('/api/v1/words', (req, res) => {
+    console.log("GET /api/v1/words called");
+
+  const length = parseInt(req.query.length);
+
+  // Filtra solo si length está definido y es un número válido
+  let filteredWords = words;
+  if (!isNaN(length)) {
+    filteredWords = words.filter(w => w.length === length);
+  }
+
+  if (filteredWords.length > 0) {
+    const randomIndex = Math.floor(Math.random() * filteredWords.length);
+    res.status(200).json({ word: filteredWords[randomIndex] });
+  } else {
+    res.status(404).json({ error: "No hay palabras con esa longitud" });
+  }
+});
 
 // Iteración 3
 
