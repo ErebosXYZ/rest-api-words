bca4f51e895ef65e609b78e2d5885e87fae46ffb
ErebosXYZ <robertmaynou@gmail.com>
2025-06-02 21:40:55 +0200

    all done

--- index.js
+++ index.js
@@ -46,24 +46,24 @@ app.use(cors());
 
 // Iteración 2
 
-app.get('/api/v1/words', (req, res) => {
-  console.log("GET /api/v1/words called");
+// app.get('/api/v1/words', (req, res) => {
+//   console.log("GET /api/v1/words called");
 
-  const length = parseInt(req.query.length);
+//   const length = parseInt(req.query.length);
 
-  // Filtra solo si length está definido y es un número válido
-  let filteredWords = words;
-  if (!isNaN(length)) {
-    filteredWords = words.filter(w => w.length === length);
-  }
+//   // Filtra solo si length está definido y es un número válido
+//   let filteredWords = words;
+//   if (!isNaN(length)) {
+//     filteredWords = words.filter(w => w.length === length);
+//   }
 
-  if (filteredWords.length > 0) {
-    const randomIndex = Math.floor(Math.random() * filteredWords.length);
-    res.status(200).json({ word: filteredWords[randomIndex] });
-  } else {
-    res.status(404).json({ error: "No hay palabras con esa longitud" });
-  }
-});
+//   if (filteredWords.length > 0) {
+//     const randomIndex = Math.floor(Math.random() * filteredWords.length);
+//     res.status(200).json({ word: filteredWords[randomIndex] });
+//   } else {
+//     res.status(404).json({ error: "No hay palabras con esa longitud" });
+//   }
+// });
 
 // Iteración 3
 
@@ -98,10 +98,35 @@ app.get('/api/v2/words', async (req, res) => {
   }
 });
 
+// Bonus 2
+
+app.get('/api/v1/words', (req, res) => {
+  try {
+    res.status(200).json(words);
+  } catch (error) {
+    res.status(500).json({ error: "Error procesando la petición GET" })
+  }
+});
+
+// Bonus 3
+
+app.get('/api/v1/words/:index', (req, res) => {
+  const wordsIndex = parseInt(req.params.index);
+
+
+  // res.status(200).json(words[wordsIndex]);
+
+  if (wordsIndex < 0 || isNaN(wordsIndex) || wordsIndex > words.length) {
+    res.status(400).json({ error: "Could not find index" })
+  } else {
+    res.status(200).json(words[wordsIndex]);
+
+  }
+});
 
 // 404 para rutas no existentes
 app.use((req, res) => {
-  res.status(404).json({ error: "Endpoint not found" });  
+  res.status(404).json({ error: "Endpoint not found" });
 });
 
 // Iniciar servidor
